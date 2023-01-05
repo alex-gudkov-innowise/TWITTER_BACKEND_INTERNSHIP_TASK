ab26b8c7202ec50a3ceb320181aa932a37edcf15
alex-gudkov-innowise <alex.gudkov@innowise-group.com>
2023-01-05 14:58:41 +0300

    Set up TypeORM with migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { typeOrmModuleOptions } from 'ormconfig';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-    imports: [],
+    imports: [TypeOrmModule.forRootAsync(typeOrmModuleOptions)],
     controllers: [AppController],
     providers: [AppService],
 })
