2cec5c7e2d76f44a9239a296e99f42e1be284ae0
alex-gudkov-innowise <alex.gudkov@innowise-group.com>
2023-01-28 23:42:32 +0300

    Separate data source from typeorm config file

--- configs/typeorm-config.ts
+++ configs/typeorm-config.ts
@@ -1,32 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
-import { config } from 'dotenv';
-import { DataSource, DataSourceOptions } from 'typeorm';
 
-config();
-const configService = new ConfigService();
-
-const dataSourceOptions: DataSourceOptions = {
-    type: 'postgres',
-    host: configService.get<string>('TYPEORM_HOST'),
-    port: configService.get<number>('TYPEORM_PORT'),
-    username: configService.get<string>('TYPEORM_USERNAME'),
-    password: configService.get<string>('TYPEORM_PASSWORD'),
-    database: configService.get<string>('TYPEORM_DATABASE'),
-    entities: ['./dist/**/*.entity.js'],
-    synchronize: false,
-    migrations: ['./dist/migrations/*.js'],
-    migrationsTableName: 'migrations',
-};
+import { dataSourceOptions } from './data-source';
 
 @Injectable()
 export class TypeOrmConfig implements TypeOrmOptionsFactory {
     public createTypeOrmOptions(): TypeOrmModuleOptions {
-        return dataSourceOptions;
+        return {
+            ...dataSourceOptions,
+        };
     }
 }
-
-export const dataSource = new DataSource(dataSourceOptions);
-
-// Use environment variables in typeorm-config.ts 
