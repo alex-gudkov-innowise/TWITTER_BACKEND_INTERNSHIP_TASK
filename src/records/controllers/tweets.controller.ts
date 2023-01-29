2511273fb95a40a8c2a43c3ba4fc6ce54bf803cc
alex-gudkov-innowise <alex.gudkov@innowise-group.com>
2023-01-29 13:14:29 +0300

    Add ability to read tweets definition

--- src/records/controllers/tweets.controller.ts
+++ src/records/controllers/tweets.controller.ts
@@ -1,10 +1,13 @@
+import { ForbiddenError } from '@casl/ability';
 import { Body, Controller, Delete, Get, Param, Post, UploadedFiles, UseGuards, UseInterceptors } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
 
 import { CheckAbilityDecorator } from 'src/decorators/check-ability.decorator';
+import { CurrentUserRolesDecorator } from 'src/decorators/current-user-roles.decorator';
 import { CurrentUserDecorator } from 'src/decorators/current-user.decorator';
 import { AbilityGuard } from 'src/guards/ability.guard';
 import { AuthGuard } from 'src/guards/auth.guard';
+import { CaslAbilityFactory } from 'src/restrictions/casl-ability.factory';
 import { UsersEntity } from 'src/users/entities/users.entity';
 import { UsersService } from 'src/users/services/users.service';
 
@@ -14,7 +17,11 @@ import { TweetsService } from '../services/tweets.service';
 @UseGuards(AuthGuard)
 @Controller('tweets')
 export class TweetsController {
-    constructor(private readonly tweetsService: TweetsService, private readonly usersService: UsersService) {}
+    constructor(
+        private readonly tweetsService: TweetsService,
+        private readonly usersService: UsersService,
+        private readonly caslAbilityFactory: CaslAbilityFactory,
+    ) {}
 
     @Post('/restriction/read/:userId')
     public async createReadingTweetsRestriction(
@@ -27,10 +34,19 @@ export class TweetsController {
     }
 
     @Get('/user/:userId')
-    @UseGuards(AbilityGuard)
-    @CheckAbilityDecorator({ action: 'read', subject: 'tweets' })
-    public async getAllUserTweets(@Param('userId') userId: string) {
+    public async getAllUserTweets(
+        @Param('userId') userId: string,
+        @CurrentUserDecorator() currentUser: UsersEntity,
+        @CurrentUserRolesDecorator() currentUserRoles: string[],
+    ) {
         const user = await this.usersService.getUserById(userId);
+        const currentUserAbility = await this.caslAbilityFactory.defineAbilityToReadTweets(
+            currentUser,
+            currentUserRoles,
+            user,
+        );
+
+        ForbiddenError.from(currentUserAbility).throwUnlessCan('read', 'tweets');
 
         return this.tweetsService.getAllUserTweets(user);
     }
--- src/restrictions/casl-ability.factory.ts
+++ src/restrictions/casl-ability.factory.ts
@@ -1,10 +1,15 @@
-import { AbilityBuilder, createMongoAbility } from '@casl/ability';
-import { Injectable } from '@nestjs/common';
+import { AbilityBuilder, MongoAbility, createMongoAbility } from '@casl/ability';
+import { Inject, Injectable } from '@nestjs/common';
+import { REQUEST } from '@nestjs/core';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { Permission } from 'src/interfaces/permission.interface';
+import { RequestWithParamsUserId } from 'src/interfaces/request-with-params.interface';
+import { RequestWithUserRoles } from 'src/interfaces/request-with-user-roles.interface';
+import { RequestWithUser } from 'src/interfaces/request-with-user.interface';
 import { UsersEntity } from 'src/users/entities/users.entity';
+import { UsersService } from 'src/users/services/users.service';
 
 import { defaultPermissions } from './default-permissions';
 import { RestrictionsEntity } from './restrictions.entity';
@@ -13,7 +18,38 @@ import { RestrictionsEntity } from './restrictions.entity';
 export class CaslAbilityFactory {
     constructor(
         @InjectRepository(RestrictionsEntity) private readonly restrictionsRepository: Repository<RestrictionsEntity>,
+        @Inject(REQUEST) private readonly request: RequestWithUser & RequestWithParamsUserId & RequestWithUserRoles,
+        private readonly usersService: UsersService,
     ) {}
+
+    public async defineAbilityToReadTweets(
+        targetUser: UsersEntity,
+        targetUserRoles: Array<string>,
+        initiatorUser: UsersEntity,
+    ) {
+        const { build, can } = new AbilityBuilder<MongoAbility>(createMongoAbility);
+
+        if (targetUserRoles.includes('admin')) {
+            can('read', 'tweets');
+
+            return build();
+        }
+
+        const isRestrictionExist = await this.restrictionsRepository.exist({
+            where: {
+                targetUser,
+                initiatorUser,
+                action: 'read',
+                subject: 'tweets',
+            },
+        });
+
+        if (!isRestrictionExist) {
+            can('read', 'tweets');
+        }
+
+        return build();
+    }
 
     public async defineAbility(targetUser: UsersEntity, initiatorUser: UsersEntity, targetUserRoles: string[]) {
         const { build, can, cannot } = new AbilityBuilder(createMongoAbility);
